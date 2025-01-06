7298020acc63315e72d2a6507ef85be88633f286
sajad <sajad@mail.com>
2025-01-06 16:31:21 +0100

    Initial commit

---
+++ app/src/main/java/com/sajad/mytrainingtracker/ui/workout/WorkoutFragment.kt
@@ -0,0 +1,25 @@
+package com.sajad.mytrainingtracker.ui.workout
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sajad.mytrainingtracker.R
+
+
+class WorkoutFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout, container, false)
+    }
+}
